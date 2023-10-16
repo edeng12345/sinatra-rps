4370fac038dc04351b7f848a6d2525b408770036
edeng12345 <146144686+edeng12345@users.noreply.github.com>
2023-10-16 20:40:48 +0000

    rps

--- app.rb
+++ app.rb
@@ -7,3 +7,23 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock","paper","scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else @comp_move == "scissors"
+    @outcome= "won"
+  end
+ 
+  erb(:zebra)
+end
+
+get("/paper") do
+  erb(:giraffe)
+end
